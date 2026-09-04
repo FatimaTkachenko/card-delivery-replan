e1fedf58afbc22067455d31561a6d362db534518
Fatima <fat.tka4enko2014@yandex.ru>
2026-09-04 16:12:08 +0300

    Final version: fix date change for replanning and CI setup

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -12,6 +12,11 @@ public class CardDeliveryTest {
 
     @BeforeAll
     static void setUpAll() {
+        // Автоматически определяем ОС и настраиваем драйвер
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("linux")) {
+            System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+        }
         Configuration.browser = "chrome";
         Configuration.headless = true;
         Configuration.timeout = 10000;
